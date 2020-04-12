e6b836c1db49ba993384a504b0543eecfbd34d11
Jayant Jaiswal <jayant.jaiswal@grabtaxi.com>
2020-04-12 14:20:44 +0530

    Initial Commit

---
+++ MyLoggingFramework/MyLoggingFramework.h
@@ -0,0 +1,19 @@
+//
+//  MyLoggingFramework.h
+//  MyLoggingFramework
+//
+//  Created by Jayant Jaiswal on 12/04/20.
+//  Copyright © 2020 GrabTaxi Pte Ltd. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for MyLoggingFramework.
+FOUNDATION_EXPORT double MyLoggingFrameworkVersionNumber;
+
+//! Project version string for MyLoggingFramework.
+FOUNDATION_EXPORT const unsigned char MyLoggingFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MyLoggingFramework/PublicHeader.h>
+
+
